79ffb20c898a440fc5df0e6a10a720dbd9aa5855
leehyeeun <leehe0430@gmail.com>
2022-03-22 22:18:29 +0900

    React Velopert CH17 Redux with React - TodosContainer with Hooks

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,45 +1,28 @@
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Todos from "../components/Todos";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
 
-const TodosContainer = ({
-  input, // input에 입력되는 텍스트
-  todos, //할 일 목록이 들어 있는 객체
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const input = useSelector((state) => state.todos.input);
+  const todos = useSelector((state) => state.todos.todos);
+
+  const dispatch = useDispatch();
+
+  const onChangeInput = (input) => dispatch(changeInput(input));
+  //Todos 컴포넌트에서 onChangeInput에 인풋 파라미터를 넣어준다.
+  const onInsert = (text) => dispatch(insert(text));
+  const onToggle = (id) => dispatch(toggle(id));
+  const onRemove = (id) => dispatch(remove(id));
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-const mapStateToProps = (state) => ({
-  input: state.todos.input,
-  todos: state.todos.todos,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  changeInput: (input) => {
-    dispatch(changeInput(input));
-  },
-  insert: (text) => {
-    dispatch(insert(text));
-  },
-  toggle: (id) => {
-    dispatch(toggle(id));
-  },
-  remove: (id) => {
-    dispatch(remove(id));
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
+export default TodosContainer;
